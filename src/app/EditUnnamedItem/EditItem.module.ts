a1a675f397d66bde3b6da7fc4e65acfe482a669d
joshuahazelwood <jlhaze04@louisville.edu>
2021-05-14 15:52:41 -0400

    1 file modified, 2 added, 2 deleted

--- src/app/EditUnnamedItem/EditItem.module.ts
+++ src/app/EditUnnamedItem/EditItem.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { EditUnnamedItemComponent } from '.EditableList/EditUnnamedItem.component';
+import { EditUnnamedItemComponent } from './EditableList/EditableList.component';
 
 import { UnnamedRoutingModule } from './EditUnnamedItem-routing.module';
 
